92812f1c0ff6846fc1de6e6af8788d85ac01207c
Andrei Nechaev <anechaev@nuxeo.com>
2017-01-27 16:43:52 -0500

    adding ZK env var

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -7,6 +7,7 @@
 # * LOG_RETENTION_HOURS: the minimum age of a log file in hours to be eligible for deletion (default is 168, for 1 week)
 # * LOG_RETENTION_BYTES: configure the size at which segments are pruned from the log, (default is 1073741824, for 1GB)
 # * NUM_PARTITIONS: configure the default number of log partitions per topic
+# * ZK: zookeeper address
 
 # Configure advertised host/port if we run in helios
 if [ ! -z "$HELIOS_PORT_kafka" ]; then
@@ -24,6 +25,11 @@ if [ ! -z "$ADVERTISED_PORT" ]; then
     sed -r -i "s/#(advertised.port)=(.*)/\1=$ADVERTISED_PORT/g" $KAFKA_HOME/config/server.properties
 fi
 
+# Set zk
+if [ ! -z "$ZK" ]; then
+    ZK=localhost
+fi
+
 # Set the zookeeper chroot
 if [ ! -z "$ZK_CHROOT" ]; then
     # wait for zookeeper to start up
@@ -38,7 +44,7 @@ if [ ! -z "$ZK_CHROOT" ]; then
     }
 
     # configure kafka
-    sed -r -i "s/(zookeeper.connect)=(.*)/\1=localhost:2181\/$ZK_CHROOT/g" $KAFKA_HOME/config/server.properties
+    sed -r -i "s/(zookeeper.connect)=(.*)/\1=/$ZK/:2181\/$ZK_CHROOT/g" $KAFKA_HOME/config/server.properties
 fi
 
 # Allow specification of log retention policies
